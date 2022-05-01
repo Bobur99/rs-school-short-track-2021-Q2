79c692578a476a584893820d5ed1205c785b942d
Bobur <rasulev.bobur.1999@mail.ru>
2022-05-02 02:30:02 +0500

    js task test

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -6,16 +6,38 @@
  * @return {Number}
  *
  * @example
- * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
- * ]
+ matrix = [
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+   [2, 0, 3, 3]
+  ]
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+
+const matrix = [
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+  [2, 0, 3, 3],
+];
+
+function matrixElementsSum(matrix) {
+  let sum = 0;
+  for (let row = 0; row < matrix.length; row++) {
+    const currRow = matrix[row];
+    for (let col = 0; col < currRow.length; col++) {
+      if (currRow[col] === 0) {
+        if (row + 1 < matrix.length) {
+          matrix[row + 1][col] = 0;
+        }
+      } else {
+        sum += currRow[col];
+      }
+    }
+  }
+  return sum;
 }
+
+matrixElementsSum(matrix);
 
 module.exports = getMatrixElementsSum;
